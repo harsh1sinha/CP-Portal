8632d82fa1749f5877dffb0a2307fe4cd2d50340
Harsh Sinha <85688366+harsh1sinha@users.noreply.github.com>
2023-10-16 23:03:39 +0530

    authentication added

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+export default NextAuth({
+    providers: [
+        GoogleProvider({
+          clientId: process.env.Google_Client_ID ,
+          clientSecret: process.env.Google_Client_Secret
+        })
+      ]
+})
